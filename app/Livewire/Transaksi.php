cce3f2b5673aa5b6c2bff0f7fc72518ecdbaa676
Kenzo75 <ff5146372@gmail.com>
2024-05-24 13:07:22 +0700

    first commit

--- app/Livewire/Transaksi.php
+++ app/Livewire/Transaksi.php
@@ -2,10 +2,34 @@
 
 namespace App\Livewire;
 
+use App\Models\DetilTransaksi;
+use App\Models\Transaksi as ModelsTransaksi;
 use Livewire\Component;
 
 class Transaksi extends Component
 {
+    public $kode, $total, $bayar, $kembalian, $totalSemuaBelanja;
+    public $transaksiAktif;
+
+    public function transaksiBaru(){
+        $this->reset();
+        $this->transaksiAktif = new ModelsTransaksi();
+        $this->transaksiAktif->kode =  'INV'. date('YmdHis');
+        $this->transaksiAktif->total = 0;
+        $this->transaksiAktif->status = 'pending';
+        $this->transaksiAktif->save();
+    }
+
+    public function batalTransaksi(){
+        if ($this->transaksiAktif) {
+        $detilTransaksi = DetilTransaksi::where('transaksi_id', $this->transaksiAktif->id)->get();
+        foreach ($detilTransaksi as $detil) {
+            $detil->delete();
+        }
+        $this->transaksiAktif->delete();
+        }
+        $this->reset();
+    }
     public function render()
     {
         return view('livewire.transaksi');
--- resources/views/livewire/transaksi.blade.php
+++ resources/views/livewire/transaksi.blade.php
@@ -1,3 +1,62 @@
 <div>
-    {{-- Be like water. --}}
+    <div class="container">
+        <div class="row mt-2">
+            <div class="col-12">
+                @if (!$transaksiAktif)
+                <button class="btn btn-primary" wire::click='transaksiBaru'>Transaksi Baru</button>
+                @else
+                <button class="btn btn-danger" wire::click='batalTransaksi'>Batalkan Transaksi</button>
+                @endif
+                <button class="btn btn-info" wire:loading>Loading...</button>
+            </div>
+        </div>
+            <div class="row mt-2">
+            <div class="col-8">
+            <div class="card  border-primary">
+            <div class="card-body">
+                <h4 class="card-title">No Invoice:</h4>
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Kode Barang</th>
+                            <th>Nama Barang</th>
+                            <th>Harga</th>
+                            <th>Qty</th>
+                            <th>Subtotal</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                </table>
+        </div>
+        </div>
+        </div>
+            <div class="col-4">
+            <div class="card  border-primary">
+            <div class="card-body">
+                <h4 class="card-title">Total Biaya</h4>
+                <div class="d-flex justify-content-between">
+                    <span>Rp. </span>
+                    <span>{{number_format('1234567', 2, '.', ',') }}0</span>
+                </div>
+            </div>
+            </div>
+            <div class="card  border-primary mt-2">
+                <div class="card-body">
+                <h4 class="card-title">Bayar</h4>
+                <input type="number" class="form-control" placeholder="Bayar">
+            </div>
+            </div>
+            </div>
+            <div class="card  border-primary mt-2">
+                 <div class="card-body">
+                <h4 class="card-title">Kembalian</h4>
+                <span>Rp. </span>
+                    <span>{{number_format('1234567', 2, '.', ',') }}0</span>
+            </div>
+            </div>
+            <button class="btn btn-success mt-2 w-100">Bayar</button>
+            </div>
+        </div>
+    </div>
 </div>
